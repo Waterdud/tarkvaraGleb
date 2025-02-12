81dbc9d0e7a03d06f3defbf6b90ee1174084d096
Bogdan Sergachev <bsergachev@gmail.com>
2025-02-12 14:00:44 +0200

    Added new stylesheet, updated script, and changed descriptions' font

--- soberpaev/index.js
+++ soberpaev/index.js
@@ -1,9 +1,20 @@
-// index.js
-document.getElementById("showPerson").addEventListener("click", function() {
-    var card = document.getElementById("personCard");
-    if (card.classList.contains("hidden")) {
-        card.classList.remove("hidden");
-    } else {
-        card.classList.add("hidden");
-    }
+document.querySelectorAll('button').forEach((button, index) => {
+    button.addEventListener("click", function() {
+        const card = document.getElementById(`personCard${index + 1}`);
+
+        if (card.classList.contains("hidden")) {
+            card.classList.remove("hidden");
+            card.style.opacity = 0;
+            setTimeout(() => {
+                card.style.transition = "opacity 0.5s";
+                card.style.opacity = 1;
+            }, 10);
+        } else {
+            card.style.transition = "opacity 0.5s";
+            card.style.opacity = 0;
+            setTimeout(() => {
+                card.classList.add("hidden");
+            }, 500);
+        }
+    });
 });

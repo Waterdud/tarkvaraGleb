9c76c17d64221ff80265dad5437c477537acf783
Waterdude <153904865+Waterdud@users.noreply.github.com>
2025-02-12 11:48:42 +0200

    Add files via upload

---
+++ soberpaev/index.js
@@ -0,0 +1,9 @@
+// index.js
+document.getElementById("showPerson").addEventListener("click", function() {
+    var card = document.getElementById("personCard");
+    if (card.classList.contains("hidden")) {
+        card.classList.remove("hidden");
+    } else {
+        card.classList.add("hidden");
+    }
+});
